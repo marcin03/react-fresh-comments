15ced939079aeab26e5906104617d4a73edac404
marcin03 <marcinmulawa92@gmail.com>
2019-12-09 01:08:46 +0100

    Updates

---
+++ precache-manifest.fd556371da3385b36720427b8d195595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5e07b7428e0bc924b35b8b536a5548",
+    "url": "/react-fresh-comments/index.html"
+  },
+  {
+    "revision": "61dd32d35ba3d6a34529",
+    "url": "/react-fresh-comments/static/css/main.4f06390f.chunk.css"
+  },
+  {
+    "revision": "331270b5d357e2226847",
+    "url": "/react-fresh-comments/static/js/2.8f6cea29.chunk.js"
+  },
+  {
+    "revision": "61dd32d35ba3d6a34529",
+    "url": "/react-fresh-comments/static/js/main.9f7878ba.chunk.js"
+  },
+  {
+    "revision": "67f0b99a707723b771ad",
+    "url": "/react-fresh-comments/static/js/runtime-main.15f8bd86.js"
+  }
+]);
